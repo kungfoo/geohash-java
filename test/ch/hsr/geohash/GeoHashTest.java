3a5f854231fb9c0d98dd3204eed22bfd70d12b0f
Silvio Heuberger <silvio.heuberger@hsr.ch>
2010-03-29 16:08:57 +0200

    cleaned imports.

--- test/ch/hsr/geohash/GeoHashTest.java
+++ test/ch/hsr/geohash/GeoHashTest.java
@@ -1,6 +1,5 @@
 package ch.hsr.geohash;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
