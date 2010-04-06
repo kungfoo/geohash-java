028671213f85b9acc2af2d528bd2e128cbf57f58
Silvio Heuberger <silvio.heuberger@hsr.ch>
2010-04-06 09:36:58 +0200

    - added copyright and license header to source files.

--- src/ch/hsr/geohash/WGS84Point.java
+++ src/ch/hsr/geohash/WGS84Point.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright 2010, Silvio Heuberger @ IFS www.ifs.hsr.ch
+ *
+ * This code is release under the LGPL license.
+ * You should have received a copy of the license
+ * in the LICENSE file. If you have not, see
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 package ch.hsr.geohash;
 
 /**
--- test/ch/hsr/geohash/WGS84PointTest.java
+++ test/ch/hsr/geohash/WGS84PointTest.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2010, Silvio Heuberger @ IFS www.ifs.hsr.ch
+ *
+ * This code is release under the LGPL license.
+ * You should have received a copy of the license
+ * in the LICENSE file. If you have not, see
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ */
+
 package ch.hsr.geohash;
 
 import static org.junit.Assert.*;
