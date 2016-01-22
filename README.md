c9957767bbf95c7d4e7f7e6c0c8bfd732a8bea3c
Silvio Heuberger <silvio.heuberger@ergon.ch>
2016-01-22 13:06:01 +0100

    Change license notice in source files

--- README.md
+++ README.md
@@ -139,6 +139,6 @@ Version 1.0.1
 License
 -------
 
-This code has been placed under the Apache 2 License.
+This code has been placed under the Apache License 2.0.
 See the LICENSE file for more information.
 Please contribute improvements and bug fixes back via github.
--- src/main/java/ch/hsr/geohash/util/GeoHashSizeTable.java
+++ src/main/java/ch/hsr/geohash/util/GeoHashSizeTable.java
@@ -1,10 +1,10 @@
 /*
  * Copyright 2010, Silvio Heuberger @ IFS www.ifs.hsr.ch
  *
- * This code is release under the LGPL license.
+ * This code is release under the Apache License 2.0.
  * You should have received a copy of the license
  * in the LICENSE file. If you have not, see
- * http://www.gnu.org/licenses/lgpl-3.0.txt
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 package ch.hsr.geohash.util;
 
